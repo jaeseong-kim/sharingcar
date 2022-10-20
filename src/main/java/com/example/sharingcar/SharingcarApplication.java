56188c8579caa87780729ae65b8d4988cb9c3f94
jaeseong-kim <doctorwho123@naver.com>
2022-10-20 15:45:50 +0900

    first commit

---
+++ src/main/java/com/example/sharingcar/SharingcarApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sharingcar/SharingcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
