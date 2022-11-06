fd8547478dc683d70038c3cb25159624fde106d0
jaeseong-kim <doctorwho123@naver.com>
2022-11-06 18:14:32 +0900

    수정사항 없음

--- src/main/java/com/example/sharingcar/SharingCarApplication.java
+++ src/main/java/com/example/sharingcar/SharingCarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SharingcarApplication {
+public class SharingCarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SharingcarApplication.class, args);
+		SpringApplication.run(SharingCarApplication.class, args);
 	}
 
 }
