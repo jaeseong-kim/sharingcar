4f393830c65dafa0c1cb0c1edcce65ce11207154
jaeseong-kim <doctorwho123@naver.com>
2022-11-07 20:11:52 +0900

    JasyptConfiguration 설정 클래스 추가

---
+++ src/main/java/com/example/sharingcar/configuration/JasyptConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.sharingcar.configuration;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfiguration {
+
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor(){
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+		config.setPassword("tonnkatzu");
+		config.setAlgorithm("PBEWITHMD5ANDDES");
+		config.setPoolSize("1");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+
+		return encryptor;
+	}
+}
