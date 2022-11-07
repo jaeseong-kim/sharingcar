c2ced057a5880072cc819985e4eef0bbd47e0ee1
jaeseong-kim <doctorwho123@naver.com>
2022-11-07 18:19:38 +0900

    로그인 예외 처리 로직 변경

--- src/main/java/com/example/sharingcar/configuration/UserAuthenticationFailureHandler.java
+++ src/main/java/com/example/sharingcar/configuration/UserAuthenticationFailureHandler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -15,10 +14,8 @@ public class UserAuthenticationFailureHandler extends SimpleUrlAuthenticationFai
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException exception) throws IOException, ServletException {
 
-		String msg = "로그인에 실패했습니다.";
-		if (exception instanceof BadCredentialsException) {
-			msg = "아이디 및 비밀번호가 일치하지 않습니다.";
-		}
+		String msg = "아이디 및 비밀번호가 일치하지 않습니다.";
+
 		if (exception instanceof InternalAuthenticationServiceException) {
 			msg = "이메일 인증 후 이용해주세요.";
 		}
