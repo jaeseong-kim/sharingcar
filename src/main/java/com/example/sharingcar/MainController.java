49bc51594f6c02017afb29033adeba273e41f279
jaeseong-kim <doctorwho123@naver.com>
2022-11-14 13:24:26 +0900

    index page

---
+++ src/main/java/com/example/sharingcar/MainController.java
@@ -0,0 +1,22 @@
+package com.example.sharingcar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String index() {
+
+		return "index";
+	}
+
+
+	@GetMapping("/error/denied")
+	public String error() {
+
+		return "error/denied";
+	}
+}
